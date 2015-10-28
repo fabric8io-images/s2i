9706287fad6edb0ab0bcaadab0a342129934b3ee
Roland Huß <roland@jolokia.org>
2015-10-28 13:15:22 +0100

    Fixed for standalone usage.

--- karaf/deploy-and-run.sh
+++ karaf/deploy-and-run.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-DIR=${DEPLOYMENTS_DIR:-/deployments}
-
-# Output will result in /opt/jboss folder
-DEPLOY_DIR=/opt/jboss
-mkdir -p ${DEPLOY_DIR}
+DIR="/deployments"
 
 echo "Checking for *.tar.gz in $DIR"
-if [ ! -d $DIR ]; then
-  echo "Missing assembly archive directory ${DIR}..."
-  exit 1
-fi
-
 # there should be only one *.tar.gz in ${DIR}
 NUM_ARCHIVE_FILES=`ls -1 ${DIR} | grep "^.*.tar.gz$" | wc -l`
 if [ $NUM_ARCHIVE_FILES -ne 1 ]; then
@@ -21,12 +12,9 @@ fi
 KARAF_ASSEMBLY_ARCHIVE=`ls -1 ${DIR}/*.tar.gz`
 
 # extract custom assembly to DEPLOY_DIR
-tar xzf "$KARAF_ASSEMBLY_ARCHIVE" -C ${DEPLOY_DIR}
+tar xzf "$KARAF_ASSEMBLY_ARCHIVE" -C ${DIR}
 KARAF_ASSEMBLY_DIR=${KARAF_ASSEMBLY_ARCHIVE%.tar.gz}
-ln -s "${DEPLOY_DIR}/${KARAF_ASSEMBLY_DIR##*/}" "${DEPLOY_DIR}/karaf"
-
-# send log output to stdout
-sed -i 's/^\(.*rootLogger.*\)out/\1stdout/' ${DEPLOY_DIR}/karaf/etc/org.ops4j.pax.logging.cfg
+ln -s "${DIR}/${KARAF_ASSEMBLY_DIR##*/}" "${DIR}/karaf"
 
 # Launch Karaf using S2I script
 exec /usr/local/s2i/run
