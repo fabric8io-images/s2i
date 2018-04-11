aa476aeaffcfbd735023e5cdb650a1eef277eb7f
Michael Vorburger <vorburger@redhat.com>
2018-04-11 02:34:30 +0200

    fix test.sh (now it works)

--- test.sh
+++ test.sh
@@ -5,10 +5,21 @@ cd java ; fish-pepper ; cd ..
 
 docker build java/images/jboss/ -t fabric8/s2i-java
 
-s2i build --copy java/images/example fabric8/s2i-java fabric8/s2i-java-example
+s2i build --copy java/example fabric8/s2i-java fabric8/s2i-java-example
 
-docker run -p 8080:8080 fabric8/s2i-java-example
+CONTAINER_ID=$(docker run -d -p 8080:8080 fabric8/s2i-java-example)
 
-# TODO how to do simplest possible HTTP GET http://localhost:8080 and fail/pass?
-# TODO how to best stop docker again
-# kill $!
+# sleep 1s is required because after docker run returns, the container is up but our server may not quite be yet
+sleep 1
+
+HTTP_REPLY=$(curl http://localhost:8080)
+
+docker rm -f "$CONTAINER_ID"
+
+if [ "$HTTP_REPLY" = 'hello, world' ]; then
+  echo "TEST PASSED"
+  exit 0
+else
+  echo "TEST FAILED"
+  exit -123
+fi
