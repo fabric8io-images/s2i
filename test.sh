7cd59dc394036192bfdfa0d9b40016a367b33441
Michael Vorburger <vorburger@redhat.com>
2018-04-18 13:17:40 +0200

    use dynamic port mapping instead of fixed 8080 in test.sh

--- test.sh
+++ test.sh
@@ -11,12 +11,13 @@ s2i build --copy java/example fabric8/s2i-java fabric8/s2i-java-example
 # Now rebuild incrementally, it should not re-download .m2
 s2i build --copy java/example fabric8/s2i-java fabric8/s2i-java-example --incremental
 
-CONTAINER_ID=$(docker run -d -p 8080:8080 fabric8/s2i-java-example)
+CONTAINER_ID=$(docker run --name s2i-java-example-test -d -p 8080 fabric8/s2i-java-example)
 
 # sleep is required because after docker run returns, the container is up but our server may not quite be yet
 sleep 5
 
-HTTP_REPLY=$(curl --silent --show-error http://localhost:8080)
+HTTP_PORT="$(docker ps|grep s2i-java-example-test|sed 's/.*0.0.0.0://g'|sed 's/->.*//g')"
+HTTP_REPLY=$(curl --silent --show-error http://localhost:$HTTP_PORT)
 
 docker rm -f "$CONTAINER_ID"
 
