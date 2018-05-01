1e5a1ac6795b329d25cf02a8be4915570c823987
Michael Vorburger <vorburger@redhat.com>
2018-05-01 23:47:32 +0200

    refactor test.sh for more tests
    so that more coverage can be added to it easily;
    one for binary is coming up in the next commit;
    one for gradle in the respective PR.

--- test.sh
+++ test.sh
@@ -5,25 +5,38 @@ cd java ; fish-pepper ; cd ..
 
 docker build java/images/jboss/ -t fabric8/s2i-java
 
-s2i build --copy java/examples/maven fabric8/s2i-java fabric8/s2i-java-example
+
+# --------------------------------------------------------------------------------------------------------------
+
+function test_container() {
+  local name=$1
+
+  CONTAINER_ID=$(docker run --name ${name}-test -d -p 8080 fabric8/${name})
+
+  # sleep is required because after docker run returns, the container is up but our server may not quite be yet
+  sleep 5
+
+  HTTP_PORT="$(docker ps|grep ${name}-test|sed 's/.*0.0.0.0://g'|sed 's/->.*//g')"
+  HTTP_REPLY=$(curl --silent --show-error http://localhost:$HTTP_PORT)
+
+  docker rm -f "$CONTAINER_ID"
+
+  if [ "$HTTP_REPLY" = 'hello, world' ]; then
+    echo "TEST PASSED"
+    return 0
+  else
+    echo "TEST FAILED"
+    return -123
+  fi
+}
+
+
+# --------------------------------------------------------------------------------------------------------------
+# Maven
+
+s2i build --copy java/examples/maven fabric8/s2i-java fabric8/s2i-java-maven-example
 
 # Now rebuild incrementally, it should not re-download .m2
-s2i build --copy java/examples/maven fabric8/s2i-java fabric8/s2i-java-example --incremental
+s2i build --copy java/examples/maven fabric8/s2i-java fabric8/s2i-java-maven-example --incremental
 
-CONTAINER_ID=$(docker run --name s2i-java-example-test -d -p 8080 fabric8/s2i-java-example)
-
-# sleep is required because after docker run returns, the container is up but our server may not quite be yet
-sleep 5
-
-HTTP_PORT="$(docker ps|grep s2i-java-example-test|sed 's/.*0.0.0.0://g'|sed 's/->.*//g')"
-HTTP_REPLY=$(curl --silent --show-error http://localhost:$HTTP_PORT)
-
-docker rm -f "$CONTAINER_ID"
-
-if [ "$HTTP_REPLY" = 'hello, world' ]; then
-  echo "TEST PASSED"
-  exit 0
-else
-  echo "TEST FAILED"
-  exit -123
-fi
+test_container "s2i-java-maven-example"
